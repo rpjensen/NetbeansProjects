3fc709c69adf1e5af10584e6f3ff0c4a844771cb
Ryan Jensen <rjensen.professional@gmail.com>
2014-08-25 20:15:53 -0400

    Started Hw1 of csci 333 -- Data Structures

---
+++ csci333_hw1/src/csci333_hw1/Hw1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package csci333_hw1;
+
+/**
+ * Programming portion of the first homework
+ * @author Ryan Jensen
+ * @version August 31, 2014
+ */
+public class Hw1 {
+    
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
