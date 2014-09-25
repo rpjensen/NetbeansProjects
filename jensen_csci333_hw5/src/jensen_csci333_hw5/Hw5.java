31051e1340a213a96a15c557c91109cdc0019884
Ryan Jensen <rjensen.professional@gmail.com>
2014-09-24 23:21:14 -0400

    Finsished most of HW5. Yet to complete the last extra credit or testing.

--- jensen_csci333_hw5/src/jensen_csci333_hw5/Hw5.java
+++ jensen_csci333_hw5/src/jensen_csci333_hw5/Hw5.java
@@ -6,6 +6,9 @@
 
 package jensen_csci333_hw5;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Implementation and testing for CSCI 333 -- HW 5
  * @author Ryan Jensen
@@ -51,6 +54,135 @@ public class Hw5 {
             sorted[place] = value;//put the value there
             where[value]--;//the next time value comes up put it one to the left
         } 
+    }
+    
+    /**
+     * A method to find the value associated with a given order statistic in the
+     * array given. The first order statistic is the smallest value in the array.
+     * Pre-Condition: orderStat is between one and the length of the array
+     * Post-Condition: the returned value will be the nth smallest number in the array
+     * @param array the array to look through
+     * @param orderStat the order statistic to return
+     * @return the value for the given orderStat
+     */
+    public static int randomizedQuickSelect(int[] array, int orderStat){
+        if (orderStat < 1 || orderStat > array.length){throw new IllegalArgumentException("Order Stat should be between 1 and array length: " + orderStat);}
+        int[] copy = new int[array.length];
+        copy = Arrays.copyOf(array, copy.length);
+        return randomizedQuickSelect(copy, 0, copy.length-1, orderStat-1);
+    }
+    
+    /**
+     * Opposed to the pseudocode in the book, this method avoids complicated array
+     * addition for the next i value, but instead exploits the fact that the indices of lower and upper
+     * are absolute relative to the full array.  If we know that a value is in sorted place regardless
+     * of the other values and it is equal to i, we know we have found the i+1'th order
+     * statistic (zero indexing).
+     * @param array the array to search through
+     * @param lower the lower index of the sub array to consider
+     * @param upper the upper index of the sub array to consider
+     * @param i the index the order statistic would be located at in a sorted array
+     * @return the value of the i+1'th order statistic
+     */
+    private static int randomizedQuickSelect(int[] array, int lower, int upper, int i){
+        if (i < lower || i > upper){throw new IllegalArgumentException(String.format("i: %d should be between lower: %d and upper: %d\n", i, lower, upper));}
+        
+        int length = upper - lower + 1;//get the length
+        Random gen = new Random();
+        
+        if (length == 1){return array[lower];}//trivial case
+        //gen pivot between lower and upper
+        int pivot = gen.nextInt(length);//[0,length)
+        pivot += lower;//[lower,length+lower) = [lower, upper + 1) = [lower, upper]
+        //swap
+        int temp = array[pivot];
+        array[pivot] = array[upper];
+        array[upper] = temp;
+        //pivot has the index of the element in the correct place
+        pivot = partition(array, lower, upper);
+        //now test if pivot equals the order desired else recurse in one of two cases
+        if (pivot == i){return array[pivot];}
+        else if (i < pivot){
+            return randomizedQuickSelect(array, lower, pivot-1, i);
+        }
+        else {
+            return randomizedQuickSelect(array, pivot+1, upper, i);
+        }        
+    }
+    
+    /**
+     * The helper method that moves all the integers lower than the pivot (last item by default)
+     * to its left, and all the integers greater than the pivot to the right
+     * @param array the array to partition (in place)
+     * @param lower the lower index
+     * @param upper the upper index (inclusive)
+     * @return the index of the pivot value which is now in the correct sorted location
+     */
+    private static int partition(int[] array, int lower, int upper){
+        int pivot = array[upper];
+        int smallBucket = lower - 1;//highest index of small bucket
+        for (int i = lower; i < upper; i++){
+            if (array[i] <= pivot){
+                smallBucket++;//now pointing just to the right of the small bucket (large side)
+                int temp = array[smallBucket];//store the value from the large bucket
+                array[smallBucket] = array[i];//put the small element at the added spot of the small bucket
+                array[i] = temp;//put the larger value to the right between the small bucket and unvisited
+            }
+        }
+        //small bucket is pointing to the largest index of the small bucket
+        smallBucket++;//now pointing just to the right of the small bucket (large side)
+        int temp = array[upper];//store pivot value
+        array[upper] = array[smallBucket];//put a value from the large bucket into the last index
+        array[smallBucket] = temp;//put the pivot in the spot just to the right of the small bucket (middle)
+        
+        return smallBucket;//now the middle where the pivot is in the right place
+    }
+    
+    
+    public static void radixSort(int[] array){
+        if (array.length <= 1){return;}
+        
+        int highDecimal = 0;
+        for (int i = 0; i < array.length; i++){
+            String string = String.valueOf(array[i]);
+            if (string.length() > highDecimal){
+                highDecimal = string.length();
+            }
+        }
+        highDecimal--;//decimal places are numbered from zero
+        
+        int[] sorted = new int[array.length];
+        for (int i = 0; i <= highDecimal; i++){
+            radixCountingSort(array, sorted, i);
+        }
+    }
+    
+    
+    public static int[] radixCountingSort(int[] array, int[] sorted, int decimalPlace){
+        int largestValue = 9;
+        
+        int[] counts = new int[largestValue+1];//will hold the count of each possible value in [0,largestValue]
+        for (int i = 0; i < counts.length; i++){
+            counts[i] = 0;//initialize counts to zero
+        }
+        for (int i: array){
+            counts[i]++;//increase the count for each number found in the original array
+        }
+        
+        counts[0]--;//adjust the first count by 1 due to zero indexing (this adjustment will propegate in the following loop)
+        for (int i = 1; i < counts.length; i++){
+            counts[i] = counts[i] + counts[i-1];//adjust count to tell us where each value should be in the sorted array
+        }
+        int[] where = counts;//switch the varname for readibility (where[val] now gives the index of sorted to put val in)
+        
+        //now we go back to front of the original array putting the values into the correct place in sorted
+        for (int i = array.length - 1; i >= 0; i--){
+            int value = array[i];//the current value we are looking at
+            int place = where[value];//the index in sorted to put the value
+            sorted[place] = value;//put the value there
+            where[value]--;//the next time value comes up put it one to the left
+        } 
+        return sorted;
     }
     
     /**
