2ef43787ba061a28c6ba8ae7a8518b00307417a2
Ryan Jensen <rjensen.professional@gmail.com>
2014-09-27 13:45:22 -0400

    Starting my senior seminar demo project on encryption sessions.

---
+++ encryption_utility_driver/src/encryption_utility_driver/Encryption_utility_driver.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package encryption_utility_driver;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Encryption_utility_driver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
