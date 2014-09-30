69be925b9677e2916ff1cd0d7c8b9112f5b07fe2
Ryan Jensen <rjensen.professional@gmail.com>
2014-09-29 23:11:06 -0400

    Added the shell files to work with encryption between two hosts

---
+++ encryption_utility/src/encryption_utility/Host.java
@@ -0,0 +1,15 @@
+
+
+package encryption_utility;
+
+/**
+ * An encryption message host that can generate RSA keys, create a session to 
+ * another host by way of host name lookup. The session will maintain a symmetric
+ * encryption two way tunnel until terminated by both hosts. Can create digital
+ * signatures and append them inside the message body.
+ * @author Ryan Jensen
+ * @version Sep 29, 2014
+ */
+public class Host {
+
+}
